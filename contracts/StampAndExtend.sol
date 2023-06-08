b95b7e8890f85b7b8b0410783fe86f18631a098d
Bzykowa <karcia956@gmail.com>
2023-06-08 22:07:18 +0200

    work on tsa smart contract

--- contracts/StampAndExtend.sol
+++ contracts/StampAndExtend.sol
@@ -1,15 +1,105 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+import "./libraries/Curve.sol";
 
-//Only the public lists HS and C, pk A 
+contract TSA {
+    // Stored public data
+    struct TimeStampEntry {
+        Curve.G1Point X;
+        uint256 s;
+        uint256 l;
+        uint256 i;
+        uint256 data;
+    }
+    TimeStampEntry[] public HS;
+    Curve.G1Point[] public C;
+    Curve.G1Point public pubKey;
+    mapping(address => uint256) private pendingTimeStamps;
+    mapping(address => bool) internal issuers;
 
-//Implement the read operations for HS,C,A for anyone
+    // Events
+    event TimeStampRequested(address requester, uint256 data);
+    event TimeStampIssued(address requester, TimeStampEntry timestamp);
 
-//Implement events for communication with TSA (TimestampRequested,TimestampIssued)
+    // Errors
+    error NotApprovedToIssueTimeStamps(address sender);
 
-//Implement write operations to HS and C for a trusted party
+    constructor(
+        uint256[2] memory pkey,
+        uint256[2] memory c1,
+        uint256[6] memory hs0
+    ) {
+        pubKey = Curve.G1Point(pkey[0], pkey[1]);
+        C.push(Curve.G1Point(c1[0], c1[1]));
+        HS.push(
+            TimeStampEntry(
+                Curve.G1Point(hs0[0], hs0[1]),
+                hs0[2],
+                hs0[3],
+                hs0[4],
+                hs0[5]
+            )
+        );
+        issuers[msg.sender] = true;
+    }
 
-//Implement a submission of a value to be timestamped
+    // Getters
+
+    function getHS() public view returns (TimeStampEntry[] memory) {
+        return HS;
+    }
+
+    function getC() public view returns (Curve.G1Point[] memory) {
+        return C;
+    }
+
+    function getPubKey() public view returns (Curve.G1Point memory) {
+        return pubKey;
+    }
+
+    // Request a timestamp
+    function requestTimeStamp(uint256 data) external {
+        pendingTimeStamps[msg.sender] = data;
+        emit TimeStampRequested(msg.sender, data);
+    }
+
+    // Issue a timestamp
+    function issueTimeStamp(
+        uint256[6] memory ti,
+        uint256[4] memory comms,
+        address requester
+    ) external {
+        require(issuers[msg.sender]);
+        // Extend HS
+        TimeStampEntry memory HSi = TimeStampEntry(
+            Curve.G1Point(ti[0], ti[1]),
+            ti[2],
+            ti[3],
+            ti[4],
+            ti[5]
+        );
+        HS.push(HSi);
+        // Extend C with c_2i,c_2i+1
+        C.push(Curve.G1Point(comms[0], comms[1]));
+        C.push(Curve.G1Point(comms[2], comms[3]));
+        // Let the requester know that timestamp is issued
+        emit TimeStampIssued(requester, HSi);
+        delete pendingTimeStamps[requester];
+    }
+
+    // Verify the i-th timestamp
+    function verifyTimeStamp(uint256 i) external view returns (bool valid) {}
+}
+
+//Only the public lists HS and C, pk A - done
+
+//Implement the read operations for HS,C,A for anyone - done
+
+//Implement events for communication with TSA (TimestampRequested,TimestampIssued) - done
+
+//Implement write operations to HS and C for a trusted party - done
+
+//Implement a submission of a value to be timestamped - done
 
 //Implement a verification algorithm for a timestamp
--- contracts/libraries/Pedersen.sol
+++ contracts/libraries/Pedersen.sol
@@ -5,26 +5,29 @@ import "./Curve.sol";
 
 library Pedersen {
     /**
-     * @dev        Generate Pedersen Commitment C = k * G + l * H 
+     * @dev        Generate Pedersen Commitment C = k * G + l * H
      * @param      k         Secret exponent
      * @param      l         Secret exponent
      * @param      H         Generator H
      * @return     c         Pedersen commitment
      */
-    function commit(uint256 k, uint256 l, Curve.G1Point memory H) internal view returns (Curve.G1Point memory c) {
+    function commit(
+        uint256 k,
+        uint256 l,
+        uint256[2] memory H
+    ) internal view returns (uint256[2] memory c) {
         // Generate right point l * H
-        Curve.G1Point memory rt = Curve.g1mul(H, l);
+        Curve.G1Point memory rt = Curve.g1mul(Curve.G1Point(H[0], H[1]), l);
 
         // Generate left point m * g
         Curve.G1Point memory lt = Curve.g1mul(Curve.P1(), k);
 
         // Generate C = k * G + l * H
-        c = Curve.g1add(lt, rt);
-
-        return c;
+        Curve.G1Point memory C = Curve.g1add(lt, rt);
+        c[0] = C.X;
+        c[1] = C.Y;
     }
 
-    
     /**
      * @dev        Verify a Pedersen commitment
      * @param      commitment  The commitment
@@ -33,14 +36,18 @@ library Pedersen {
      * @param      l           Exponent l
      * @return     res         Success or failure
      */
-    function verify(Curve.G1Point memory commitment, Curve.G1Point memory H, Curve.G1Point memory X, uint256 l) internal view returns(bool res) {
+    function verify(
+        uint256[2] memory commitment,
+        uint256[2] memory H,
+        uint256[2] memory X,
+        uint256 l
+    ) internal view returns (bool res) {
         // Generate right point l * H
-        Curve.G1Point memory rt = Curve.g1mul(H, l);
+        Curve.G1Point memory rt = Curve.g1mul(Curve.G1Point(H[0], H[1]), l);
 
         // Generate C = X + l * H
-        Curve.G1Point memory c = Curve.g1add(X, rt);
+        Curve.G1Point memory c = Curve.g1add(Curve.G1Point(X[0], X[1]), rt);
 
-        res = (c.X == commitment.X && c.Y == commitment.Y);
+        res = (c.X == commitment[0] && c.Y == commitment[1]);
     }
-    
 }
