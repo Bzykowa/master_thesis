f8c8e2adb2c4559b5f7e960f020185812214d57b
Bzykowa <karcia956@gmail.com>
2023-06-11 20:50:46 +0200

    add connection parameters and event filtering

--- tsa/main.py
+++ tsa/main.py
@@ -5,24 +5,33 @@ from web3 import Web3
 import json
 import asyncio
 
+# Initialize the connection parameters
 with open("../api_key") as file:
-    api_key = json.loads(file.read())
+    api_key = file.read()
+
 alchemy_url = "https://eth-sepolia.g.alchemy.com/v2/" + api_key
-se_address = "paste address here"
+se_address = "0xBC5B10a20149d9A473a43Ab386a436223Da12220"
+
 with open("../abi") as file:
     se_abi = json.loads(file.read())
 
+# Connect to web3
+w3 = Web3(Web3.HTTPProvider(alchemy_url))
+print(w3.is_connected())
+contract = w3.eth.contract(address=se_address, abi=se_abi)
 
-def handle_timestamp_event(event):
+
+def handle_timestamp_event(event, se: StampExtendProtocol):
     """Handle an incoming TimeStampRequested event."""
     print(Web3.toJSON(event))
     # TODO make a timestamp and publish it
 
 
-async def log_loop(event_filter, poll_interval):
+async def log_loop(event_filter, poll_interval, se: StampExtendProtocol):
+    """Check for new events in a specified interval."""
     while True:
         for TimeStampRequested in event_filter.get_new_entries():
-            handle_timestamp_event(TimeStampRequested)
+            handle_timestamp_event(TimeStampRequested, se)
         await asyncio.sleep(poll_interval)
 
 
@@ -35,30 +44,29 @@ if __name__ == "__main__":
         "Remember it well because there is no recovery for it."
     )
     args = parser.parse_args()
+    # Initialize the encrypted data
     data = ProtocolData(args.password)
     stamp_extend = StampExtendProtocol(data)
 
-    w3 = Web3(Web3.HTTPProvider(alchemy_url))
-
-    # Print if web3 is successfully connected
-    print(w3.is_connected())
-    # Testing the contract, export the initial values
     """
+    # Testing the contract, export the initial values
     print(stamp_extend.A, stamp_extend.h,
           stamp_extend.c1, stamp_extend.HS0, sep="\n")
-    print("Timestamp for 1736..")
-    print(stamp_extend.create_timestamp(1736109339377034722022031384259527484745846388714740965905228))
+    # Test of verification
+    #print("Timestamp for 1736..")
+    #print(stamp_extend.create_timestamp(1736109339377034722022031384259527484745846388714740965905228))
     """
-    # TODO implement event handling
-    """
-    contract = w3.eth.contract(address=se_address, abi=se_abi)
-    event_filter = contract.events.PairCreated.createFilter(fromBlock='latest')
+    # TODO implement checking the backlog in the contract
+    event_filter = contract.events.TimeStampRequested.createFilter(
+        fromBlock='latest'
+    )
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
             asyncio.gather(
-                log_loop(event_filter, 2)))
+                log_loop(event_filter, 2, stamp_extend)
+            )
+        )
     finally:
         # close loop to free up system resources
         loop.close()
-    """
